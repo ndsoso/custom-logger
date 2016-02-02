fba3870fa8f7155bd2f81b26cc9c7a9f0a0f0d40
gongice <gshiwen@gmail.com>
2016-02-02 22:28:31 +0800

    update project

--- src/main/java/org/gongice/util/log/Test.java
+++ src/main/java/org/gongice/util/log/Test.java
@@ -6,21 +6,21 @@
 */
 package org.gongice.util.log;
 public class Test {
-	
+
 	//private final static Log log=new Log();
-	
+
 	public static void main(String[] args) {
-		
+
 		// 配置日志
 		final LogConfig conf=new LogConfig();
 		conf.setSubDirectory(true);// 是否分目录
 		conf.setLogId("20160128");// 日志id
 		conf.setLogDirectory("E:\\log");// 日志主目录, 依赖'分目录'和'日志id'
-		
+
 		//Log.setConf(conf);
 		//Log.init();
-		
-		
+
+
 		// 线程1 初始化
 		new Thread(new Runnable() {
 			public void run() {
@@ -32,7 +32,7 @@ public class Test {
 				}*/
 			}
 		}).start();
-		
+
 		// 线程2
 		new Thread(new Runnable() {
 			public void run() {
